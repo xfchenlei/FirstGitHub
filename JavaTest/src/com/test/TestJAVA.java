f608227f13c78d10615a26f2cffb3b7364123a61
xfchenlei <569558668@qq.com>
2014-12-10 11:10:02 +0800

    Update TestJAVA.java

--- JavaTest/src/com/test/TestJAVA.java
+++ JavaTest/src/com/test/TestJAVA.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 public class TestJAVA {
 	public static void main(String[] args) throws Exception {
-		System.out.println("远程修改的gitupdate...");
+		System.out.println("gitupdate...");
 	}
 
 }
